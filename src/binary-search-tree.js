bf7360fccfc2c6d1a63905a3e76ee948d852f3ca
Alex <vouchankaaleks@gmail.com>
2022-05-02 21:13:07 +0300

    feat: implement 3 passing

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -49,9 +49,18 @@ class BinarySearchTree {
     }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let node = this.rootA
+    while (node !== null) {
+      if(node.data === data) {
+        return node
+      } else if (data < node.data) {
+        node = node.left
+      } else {
+        node = node.right
+      }
+    }
+    return null
   }
 
   remove(data) {
