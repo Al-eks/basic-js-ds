7bdcf637781bff418c21aa2b566a17081d276b1a
Alex <vouchankaaleks@gmail.com>
2022-05-03 02:33:45 +0300

    feat: implement 3 passing

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -33,13 +33,14 @@ function removeKFromList(l, k) {
   nextNode = thisNode.next;
   while(nextNode != null){
       if(nextNode.value == k) {
-          thisNode.next = nextNode.next;
+          thisNode.next = nextNode.next
           if(thisNode.next == null)
               break;
       }
       thisNode = thisNode.next;
       nextNode = thisNode.next;       
   }
+  
   return l;
 }
 
